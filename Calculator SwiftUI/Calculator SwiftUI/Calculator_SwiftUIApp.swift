a59a7bcf1e409251ba9483d77b952d4970ccb49a
Jeevan Joshi <joshijeevan98@gmail.com>
2023-01-07 17:19:26 +0530

    setup app using swift

---
+++ Calculator SwiftUI/Calculator SwiftUI/Calculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_SwiftUIApp.swift
+//  Calculator SwiftUI
+//
+//  Created by Jeevan Chandra Joshi on 07/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
